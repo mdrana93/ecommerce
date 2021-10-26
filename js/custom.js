29f7493c03a7c1d3b8709a54b722e519fc2797ee
mdrana93 <mdrasheduzzaman2911993@gmail.com>
2021-10-26 18:13:47 +0600

    again file added

--- js/custom.js
+++ js/custom.js
@@ -25,3 +25,22 @@ function prev() {
   index = (index - 1 + slides.length) % slides.length;
   slides[index].classList.add("active");
 }
+
+document.querySelectorAll(".featured-image-1").forEach((img_1) => {
+  img_1.addEventListener("click", function () {
+    var src = img_1.getAttribute("src");
+    document.querySelector(".big-image-1").src = src;
+  });
+});
+document.querySelectorAll(".featured-image-2").forEach((img_2) => {
+  img_2.addEventListener("click", function () {
+    var src = img_2.getAttribute("src");
+    document.querySelector(".big-image-2").src = src;
+  });
+});
+document.querySelectorAll(".featured-image-3").forEach((img_3) => {
+  img_3.addEventListener("click", function () {
+    var src = img_3.getAttribute("src");
+    document.querySelector(".big-image-3").src = src;
+  });
+});
